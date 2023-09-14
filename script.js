ce10fe1b495ad069269b3598ea5bbb76d0bb00e3
DeannaLuna <125083136+DeannaLuna@users.noreply.github.com>
2023-09-14 14:46:46 +0100

    add event listeners for each div

--- script.js
+++ script.js
@@ -2,23 +2,29 @@ const boardContainer = document.querySelector(".board");
 
 let boardArray = [];
 
+const div = document.createElement("div");
+div.classList.add("divContainer")
+div.setAttribute("style", "background: transparent; border: solid 1px black");
+boardArray.push(div);
+
+
+
 // function to create div and add to array
 function createDiv(){
     const div = document.createElement("div");
     div.classList.add("divContainer")
-    div.setAttribute("style", "background: blue; border: solid 1px black");
+    div.setAttribute("style", "background: transparent; border: solid 1px black");
     boardArray.push(div);
 }
 
 
 // function to add 256 divs to array 
 function loopDiv(){
-    for (i = 0; i < 256; i++){
+    for (i = 0; i < 255; i++){
         createDiv();
     }
 }
 
-// sets colour for div 
 
 
 // for each to add each div in array to dom. add it to current empty div container
@@ -29,6 +35,24 @@ function addToDom(){
 function addDiv(){
     loopDiv();
     addToDom();
+    loopColour();
 }
 
 addDiv();
+
+
+
+
+// change the div colour
+function changeColour(div){
+    div.setAttribute("style", "background: green; border: solid 1px black");
+}
+
+
+// add event listner within a for each loop.
+
+function loopColour(){
+    boardArray.forEach(div => div.addEventListener("click", () => {
+        console.log("hi");
+    }))
+}
