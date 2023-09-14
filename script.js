773b4d749179ff53697827318689c0c092fe7fbd
DeannaLuna <125083136+DeannaLuna@users.noreply.github.com>
2023-09-14 15:00:03 +0100

    make div array global

--- script.js
+++ script.js
@@ -2,45 +2,20 @@ const boardContainer = document.querySelector(".board");
 
 let boardArray = [];
 
-const div = document.createElement("div");
-div.classList.add("divContainer")
-div.setAttribute("style", "background: transparent; border: solid 1px black");
-boardArray.push(div);
 
 
-
-// function to create div and add to array
-function createDiv(){
+function createDivArray(){
     const div = document.createElement("div");
     div.classList.add("divContainer")
     div.setAttribute("style", "background: transparent; border: solid 1px black");
     boardArray.push(div);
-}
-
-
-// function to add 256 divs to array 
-function loopDiv(){
-    for (i = 0; i < 255; i++){
-        createDiv();
-    }
-}
-
-
-
-// for each to add each div in array to dom. add it to current empty div container
-function addToDom(){
     boardArray.forEach(div => boardContainer.appendChild(div));
 }
 
-function addDiv(){
-    loopDiv();
-    addToDom();
-    loopColour();
+// globally add divs to array
+for (i = 0; i < 256; i++){
+    createDivArray();
 }
-
-addDiv();
-
-
 
 
 // change the div colour
@@ -50,9 +25,10 @@ function changeColour(div){
 
 
 // add event listner within a for each loop.
-
 function loopColour(){
-    boardArray.forEach(div => div.addEventListener("click", () => {
-        console.log("hi");
+    boardArray.forEach(div => div.addEventListener("mouseover", () => {
+        changeColour(div);
     }))
 }
+
+loopColour();
